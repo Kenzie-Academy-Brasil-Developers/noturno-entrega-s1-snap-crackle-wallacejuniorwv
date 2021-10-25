6e17e30a6e52155e3602d6e2f8d911b5b354b0bc
Wallace Junior <wallaceoliveirarn@gmail.com>
2021-10-25 19:10:41 -0300

    entrega da atividade 43

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,23 @@
-// Seu código vai aqui
+function snapCrackle(maxValue){
+    var cont =1 ;
+    for (let mult = 5;mult <= 20; mult+=5) {
+      mult5 = mult;
+      console.log(mult);
+    }
+        while (cont<=20) {
+          var resto = cont%5;
+          if (resto == 0){
+            console.log ("SnapCrackle")
+          }
+            var resto = cont%2;
+            if (resto == 0 ){
+                console.log(cont);
+           }else if (resto == 1){
+              console.log("Snap");
+            } else if(resto == 5){
+              console.log("Crackle")
+            }
+            cont = cont + 1;
+        }
+        console.log("FIM");
+    }
