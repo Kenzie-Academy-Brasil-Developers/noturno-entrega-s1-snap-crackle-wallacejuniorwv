5a91ebf3b4a4ef7cbdbec2137ba92e721b4b2a02
Wallace Junior <wallaceoliveirarn@gmail.com>
2022-02-09 17:29:53 -0300

    Update snapCrackle.js
    Correção 4.0

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,23 +1,22 @@
 function snapCrackle(maxValue) {
+    let output = "";
 
-  let numeroRetornado = " ";
+    for (let number = 1; number <= maxValue; number++) {
 
-  for (let number = 1; number <= maxValue; number++) {
+        if (number % 2 !== 0 && number % 5 === 0) {
+            output = output + "SnapCrackle";
+        } else if (number % 5 === 0) {
+            output = output + "Crackle";
+        } else if (number % 2 !== 0) {
+            output = output + "Snap";
+        } else {
+            output = output + number;
+        }
 
-    if (number % 2 !== 0 && number % 5 === 0) {
-
-      numeroRetornado = numeroRetornado + "SnapCrackle," + "";
-    } else if (number % 5 === 0) {
-
-      numeroRetornado = numeroRetornado + "Crackle, " + "";
-    } else if (number % 2 !== 0) {
-
-      numeroRetornado = numeroRetornado + "Snap, " + "";
-
-    } else {
-      numeroRetornado = numeroRetornado + number + ", ";
+        if (maxValue != number) {
+            output = output + ", ";
+        };
     }
-  }
-  return numeroRetornado;
+    return output;
 }
-console.log(snapCrackle(12));
+console.log(snapCrackle(15));
